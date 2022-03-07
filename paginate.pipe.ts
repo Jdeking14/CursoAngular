74b5f6ef47c14ce0f866ec53a28a4c9c0ef7f8a5
FCO. Javier De Quinta Rivas <javicfgs2015@gmail.com>
2022-03-07 16:59:10 +0100

    Create paginate.pipe.ts

---
+++ paginate.pipe.ts
@@ -0,0 +1,23 @@
+import memo from 'memo-decorator';
+import { pipeline } from 'stream';
+
+@Pipe({
+  name: 'Paginate'
+})
+
+export class PaginatePipe implements PipeTransform{
+  @memo((...args: any[]): string => JSON.stringify(args))
+  transform(array: any[],page_size: number | string, page_number: number): any[]{
+    if(!array.length) return []
+    if(page_size === 'all'){
+      return array
+    }
+    page_size = page_size || 5
+    page_number = page_number || 1
+
+    --page_number
+
+    // @ts-ignore
+    return array.slice(page_number * page_size, (page_number + 1) * page_size)
+  }
+}
